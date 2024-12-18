e9b6541ddc944656ba110651512a90e58e1ae18c
ssantosldn <officialsantino@gmail.com>
2024-12-18 18:00:41 +0000

    Add cache control meta tags

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+            <head>
+            <meta httpEquiv='cache-control' content='no-cache' />
+            <meta httpEquiv='expires' content='0' />
+            <meta httpEquiv='pragma' content='no-cache' />
+            </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    generateBuildId: async () => {
+        // This will add a unique timestamp to your build
+        return `build-${Date.now()}`
+    },
     images: {
       unoptimized: true,
       remotePatterns: [
